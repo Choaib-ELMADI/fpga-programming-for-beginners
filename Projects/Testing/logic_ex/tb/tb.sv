a9fbe194a6cbe77a1fe6cbb3891bb8ecf2dfb0da
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-08-06 22:56:53 +0100

    Complete the testbench module / add more notes

--- Projects/Testing/logic_ex/tb/tb.sv
+++ Projects/Testing/logic_ex/tb/tb.sv
@@ -1,10 +1,10 @@
 `timescale 1ns/100ps
 
 module tb;
-    logic [1:0] SW;
+    logic [1:0] SW;                                         // Declare tb signals
     logic [3:0] LED;
 
-    logic_ex u_logic_ex (.*);
+    logic_ex u_logic_ex (.*);                               // Connect tb signals with dut ports
     //logic_ex u_logic_ex (.SW, .LED);
     //logic_ex u_logic_ex (.SW(switch_sig), .LED(led_sig));
     //logic_ex u_logic_ex (.*, .LED(led_sig));
@@ -24,6 +24,25 @@ module tb;
         $stop;
     end
 
+    always @(SW, LED) begin
+        if (!SW[0] !== LED[0]) begin
+            $display("FAIL: NOT Gate mismatch");
+            $stop;
+        end
 
+        if (&SW[1:0] !== LED[1]) begin
+            $display("FAIL: AND Gate mismatch");
+            $stop;
+        end
 
+        if (|SW[1:0] !== LED[2]) begin
+            $display("FAIL: OR Gate mismatch");
+            $stop;
+        end
+
+        if (^SW[1:0] !== LED[3]) begin
+            $display("FAIL: XOR Gate mismatch");
+            $stop;
+        end
+    end
 endmodule
